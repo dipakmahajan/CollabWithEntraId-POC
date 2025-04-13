1913ce8352cf2b6fcdebfc302fd3b5c316b822e8
dipakmahajan <35342979+dipakmahajan@users.noreply.github.com>
2025-04-13 21:50:43 +0000

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CollabWithEntraId_POC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
